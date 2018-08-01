5bdfbbc6e4dcfe9cf417913055743d82e07c0033
Anna <anna.loshchakova@aiesec.net>
2018-08-01 23:38:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,72 @@
-var app = angular.module('WikiApp', ['ngAnimate']);
+var app = angular.module('WikiApp', []);
 app.controller('myCtrl', function($scope, $http) {
-  $("form").hide();
-  $('#formText').on('click', function(e){
-    e.preventDefault();
-    $('div>hr').animate({width: "0"},200, potom);
-    function potom(){ 
-      $("form").show();
-      $('input').focus();
-      $('#formText').animate({width: "270px"},200, potom1)}
-    function potom1() {
-      $('#up').animate({width: "20px"},200, potom2)}
-    function potom2() {
-      $('#down').animate({width: "20px"},200)}
-   });
-   $scope.search = function() {
-    $('ul').empty().hide();
-    $('#form p').hide();
-    $('#form').css('justify-content', 'flex-start').removeClass('fullHeight');  
+  const formText = $('#formText');
+  const hr = $('div>hr');
+  const form = $("form");
+  const input = $('input');
+  const up = $('#up');
+  const down = $('#down');
+  const htmlBody = $('html, body');
+  $scope.load ='Click icon to search';
+  $scope.open = () => {
+    formText.css({cursor: 'auto'});
+    hr.animate({width: "0"}, 200, function() {
+      form.css({display: 'block'});
+      input.focus();
+      formText.animate({width: "270px"}, 200, function() {
+        up.animate({width: "20px"}, 200, function() {
+        down.animate({width: "20px"}, 200);
+        });
+      });
+    }); 
+  };
+  $scope.search = () => {
+    $scope.load = 'Loading...';
+    htmlBody.css({
+      justifyContent: 'flex-start'
+    });  
+    formText.css({
+      marginTop: '10px'
+    }); 
+    getData();
+  }
+  function getData() {
+    let pattern = $("input").val();
     $scope.results = [];
-    var pattern = $("input").val();
-    var url ='https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&list=&generator=search&exsentences=1&exlimit=max&exintro=1&explaintext=1&gsrnamespace=&gsrlimit=10&gsrsearch='+pattern+'&callback=JSON_CALLBACK';
-     $http.jsonp(url)
+    let url ='https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&list=&generator=search&exsentences=1&exlimit=max&exintro=1&explaintext=1&gsrnamespace=&gsrlimit=10&gsrsearch='+pattern+'&callback=JSON_CALLBACK';
+    $http.jsonp(url)
       .success(function(data) {
-       var pages = data.query.pages;
-       angular.forEach(pages, function(v, k) {
-         $scope.results.push({title: v.title, body: v.extract, page: 'https://en.wikipedia.org/?curid=' + v.pageid})
-       });
-     });
-     }
-   $('button').click(function(e){
-    $('#form').css('justify-content', 'center').addClass('fullHeight');
-    $('#form p').show();
-    $('ul').empty();
-    $('#up').animate({width: "0px"},200, potom);
-    function potom(){
-      $('#down').animate({width: "0px"},200, potom1)}
-    function potom1(){
-      $("form").hide();
-      $('#formText').animate({width: "40px"},200, potom2)}
-    function potom2(){
-      $('div>hr').animate({width: "20"},200)}
-    e.stopPropagation();
-  });
+       if (data.hasOwnProperty('query')) {
+         let pages = data.query.pages;
+         $scope.load = '';
+         angular.forEach(pages, function(v) {
+         $scope.results.push({title: v.title, body: v.extract, page: 'https://en.wikipedia.org/?curid=' + v.pageid})
+        });
+       } else {
+         $scope.load = 'Failed to find results in English Wikipedia on your request';
+       }
+    })
+    .error(function() {
+      $scope.load = 'Error occured';
+    })
+  }
+  $scope.close = () => {
+    $scope.results = [];
+    $scope.load ='Click icon to search';
+    htmlBody.css({
+      justifyContent: 'center'
+    }); 
+    formText.css({
+      marginTop: '0'
+    }); 
+    up.animate({width: "0"}, 200, function() {
+      down.animate({width: "0"}, 200,  function() {
+        form.css({display: 'none'});
+        formText.animate({width: "30px"}, 200, function(){
+          formText.css({cursor: 'pointer'});
+          hr.animate({width: "20px"}, 200);
+        });
+      });
+    });
+  };
 });
